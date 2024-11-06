15192a8b253388b0b8a714b5b7d9fb52c5b701b9
Nicolas Cordeiro <32917658+Nicolasck-dev@users.noreply.github.com>
2024-11-05 21:22:06 -0300

    Update TelaDePesquisa.java

--- TelaDePesquisa.java
+++ TelaDePesquisa.java
@@ -1,35 +1,33 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.sql.*;
 
 public class TelaDePesquisa extends JFrame {
-
-    public final JTextField txtPesquisa;
+    public static final JTextField txtPesquisa = new JTextField(20);
     public final JButton btnPesquisar;
+    public final JButton btnReiniciarPesquisa;
 
     public final JLabel lblId;
-    public final JTextField txtId;
-    
+    public static final JTextField txtId = new JTextField(10);
+
     public final JLabel lblNome;
-    public final JTextField txtNome;
+    public static final JTextField txtNome = new JTextField(10);
 
     public final JLabel lblEmail;
-    public final JTextField txtEmail;
+    public static final JTextField txtEmail = new JTextField(10);
 
-    public final JButton btnPrimeiro;
-    public final JButton btnAnterior;
-    public final JButton btnProximo;
-    public final JButton btnUltimo;
+    public static final JButton btnPrimeiro = new JButton("<<");;
+    public static final JButton btnAnterior = new JButton("<");;
+    public static final JButton btnProximo = new JButton(">");;
+    public static final JButton btnUltimo = new JButton(">>");;
 
-    public final JLabel lblNotificacoes;
+    public static final JLabel lblNotificacoes = new JLabel("Notificações", SwingConstants.CENTER);
 
     public TelaDePesquisa() {
         super("Tela de Pesquisa");
         setLayout(new GridLayout(7,1,5,5));
 
         JPanel linhaInputPesquisa = new JPanel(new GridLayout(1,1));
-        txtPesquisa = new JTextField(20);
         linhaInputPesquisa.add(txtPesquisa);
         add(linhaInputPesquisa);
 
@@ -38,35 +36,35 @@ public class TelaDePesquisa extends JFrame {
         linhaBotaoPesquisar.add(btnPesquisar);
         add(linhaBotaoPesquisar);
 
+        btnReiniciarPesquisa = new JButton("Reiniciar Pesquisa");
+        linhaBotaoPesquisar.add(btnReiniciarPesquisa);
+        add(linhaBotaoPesquisar);
+
         JPanel linhaId = new JPanel(new GridLayout(1,2));
-        lblId = new JLabel("Id:", SwingConstants.CENTER);
-        txtId = new JTextField(10);
+        lblId = new JLabel("Id:", SwingConstants.RIGHT);
         txtId.setEditable(false);
         linhaId.add(lblId);
         linhaId.add(txtId);
         add(linhaId);
 
-        JPanel linhaNome = new JPanel( new GridLayout(1,2));
-        lblNome = new JLabel("Nome:", SwingConstants.CENTER);
-        txtNome = new JTextField(10);
+        JPanel linhaNome = new JPanel(new GridLayout(1,2));
+        lblNome = new JLabel("Nome:", SwingConstants.RIGHT);
+
         txtNome.setEditable(false);
         linhaNome.add(lblNome);
         linhaNome.add(txtNome);
         add(linhaNome);
 
         JPanel linhaEmail = new JPanel(new GridLayout(1,2));
-        lblEmail = new JLabel("Email:", SwingConstants.CENTER);
-        txtEmail = new JTextField(10);
+        lblEmail = new JLabel("Email:", SwingConstants.RIGHT);
+
         txtEmail.setEditable(false);
         linhaEmail.add(lblEmail);
         linhaEmail.add(txtEmail);
         add(linhaEmail);
 
         JPanel linhaBotoes = new JPanel(new GridLayout(1,4));
-        btnPrimeiro = new JButton("<<");
-        btnAnterior = new JButton("<");
-        btnProximo = new JButton(">");
-        btnUltimo = new JButton(">>");
+
         linhaBotoes.add(btnPrimeiro);
         linhaBotoes.add(btnAnterior);
         linhaBotoes.add(btnProximo);
@@ -74,12 +72,137 @@ public class TelaDePesquisa extends JFrame {
         add(linhaBotoes);
 
         JPanel linhaNotificacoes = new JPanel(new GridLayout(1,1));
-        lblNotificacoes = new JLabel("Notificações",SwingConstants.CENTER);
+    
         linhaNotificacoes.add(lblNotificacoes);
         add(linhaNotificacoes);
 
+        btnPrimeiro.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    NavegadorDeRegistro.vaParaPrimeiroRegistro();
+                }
+            }
+        );
+
+        btnAnterior.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    NavegadorDeRegistro.vaParaRegistroAnterior();
+                }
+            }
+        );
+
+        btnProximo.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    NavegadorDeRegistro.vaParaProximoRegistro();
+                }
+            }
+        );
+
+        btnUltimo.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    NavegadorDeRegistro.vaParaUltimoRegistro();
+                }
+            }
+        );
+
+        btnPesquisar.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    NavegadorDeRegistro.registrarPesquisa();
+                }
+            }
+        );
+
+        txtPesquisa.addKeyListener(
+            new KeyAdapter() {
+                @Override
+                public void keyReleased(KeyEvent e) {
+                    btnPesquisar.setEnabled(detectarPesquisa());
+                    if (e.getKeyCode() == 10 && txtPesquisa.getText().trim().length() > 0) {
+                        NavegadorDeRegistro.registrarPesquisa();
+                        txtPesquisa.setText(txtPesquisa.getText().trim());
+                    }            
+                }
+            }
+        );
+
+        btnReiniciarPesquisa.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    txtPesquisa.setText("");
+                    NavegadorDeRegistro.registroDePesquisa = "";
+                    NavegadorDeRegistro.clausulasDePesquisaComWhere = "";
+                    NavegadorDeRegistro.clausulasDePesquisaSemWhere = "";
+                    NavegadorDeRegistro.vaParaPrimeiroRegistro();
+                    btnPesquisar.setEnabled(false);
+                    txtPesquisa.requestFocus();
+                }
+            }
+        );
+
         setSize(300,300);
         setVisible(true);
+
+        NavegadorDeRegistro.inicializacaoDeRegistros();
+    }
+
+    public static void notificarUsuario(String strTexto) {
+        lblNotificacoes.setText(setHtmlFormat(strTexto));
+    }
+
+    public static String setHtmlFormat(String strTexto) {
+        return "<html><body>" + strTexto + "</body></html>";
+    }
+
+    public static void habilitarVoltar() {
+        btnPrimeiro.setEnabled(true);
+        btnAnterior.setEnabled(true);
+        btnProximo.setEnabled(false);
+        btnUltimo.setEnabled(false);
+    }
+
+    public static void habilitarAvancar() {
+        btnPrimeiro.setEnabled(false);
+        btnAnterior.setEnabled(false);
+        btnProximo.setEnabled(true);
+        btnUltimo.setEnabled(true);
+    }
+
+    public static void habilitarTodos() {
+        btnPrimeiro.setEnabled(true);
+        btnAnterior.setEnabled(true);
+        btnProximo.setEnabled(true);
+        btnUltimo.setEnabled(true);
+    }
+
+    public static void desabilitarTodos() {
+        btnPrimeiro.setEnabled(false);
+        btnAnterior.setEnabled(false);
+        btnProximo.setEnabled(false);
+        btnUltimo.setEnabled(false);
+    }
+
+    public static boolean detectarPesquisa() {
+        if (txtPesquisa.getText().trim().length() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public static void limparCampos() {
+        txtId.setText("");
+        txtNome.setText("");
+        txtEmail.setText("");
     }
 
     public static void main(String[] args) {
